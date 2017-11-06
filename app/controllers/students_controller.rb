653e461c69f2a1f7efa482ac28051b8943ffeda4
Gonzodamus <Dick@TheDickWard.com>
2017-11-06 15:24:40 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,32 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    # byebug
+    @student = Student.find(params[:id].to_i)
   end
+
+  def edit
+    # byebug
+    @student = Student.find(params[:id].to_i)
+  end
+
+  def update
+    @student = Student.find(params[:id].to_i)
+    redirect_to students_path(@student.id)
+  end
+
+
+
 
   private
 
     def set_student
       @student = Student.find(params[:id])
     end
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,14 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def student_active
+    if self.active == true
+      "This student is currently active."
+    elsif self.active == false
+      "This student is currently inactive."
+    else
+      "nil"
+    end
+  end
 end
